4aab903a02fec578ca011de79ad65eb187cd5604
halefalmeida <halef.dorigan@edu.unipar.br>
2022-05-07 01:21:10 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aql-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'agualogic';
+}
