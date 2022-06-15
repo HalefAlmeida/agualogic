d791d3b3a57e625d7049fc265fd240ae76070b00
halefalmeida <halef.dorigan@edu.unipar.br>
2022-06-14 21:53:48 -0300

    added API base springboot project

---
+++ back-end/src/main/java/com/halvxys/aqualogic/AqualogicApplication.java
@@ -0,0 +1,13 @@
+package com.halvxys.aqualogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqualogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqualogicApplication.class, args);
+	}
+
+}
