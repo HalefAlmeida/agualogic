a38c0f60ba5fe73f611eeb949febc61cf8e78379
halefalmeida <halef.dorigan@edu.unipar.br>
2022-07-03 23:03:36 -0300

    updated directory structure

--- back-end/src/main/java/com/halvxys/aqualogic/exceptions/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.halvxys.aqualogic.exceptions;
-
-import com.halvxys.aqualogic.utils.MessageUtils;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException() {
-        super(MessageUtils.NO_RECORDS_FOUND);
-    }
-}
--- back-end/src/main/java/com/halvxys/aqualogic/repository/AquariumRepo.java
+++
@@ -1,7 +0,0 @@
-package com.halvxys.aqualogic.repository;
-
-import com.halvxys.aqualogic.models.Aquarium;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AquariumRepo extends JpaRepository<Aquarium, Long> {
-}
